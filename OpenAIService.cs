6601f7f639223d73ae54fc6aa7b860ff4d3b6144
Miguel Serrano <superserrano420@gmail.com>
2023-09-27 19:43:15 -0500

    OpenAI API was throwing error for too many tokens when translating larger files. Creating a new chat instance for every translation fixed this.

--- OpenAIService.cs
+++ OpenAIService.cs
@@ -11,7 +11,7 @@ namespace LocalizerGUI
     internal class OpenAIService
     {
         // Holds the instructions for translation.
-        private const string TranslationInstructions = "For each word or phrase that you receive, you will translate it into several languages. After each full translation of the word or phrase, place a comma and then continue with the remainder of the languages without adding a line break. These are the languages and the output format: german,spanish,french,italian,japanese,koreana,polish,brazilian,russian,turkish,simplified chinese, and traditional chinese. You will respond with only the correctly translated and formatted output.";
+        private const string TranslationInstructions = "For each word or phrase that you receive, you will translate it into several languages. After each full translation of the word or phrase, place a comma and then continue with the remainder of the languages. Please don't add line breaks after the commas. These are the languages: german,spanish,french,italian,japanese,koreana,polish,brazilian,russian,turkish,simplified chinese,traditional chinese. This is the output format: german,spanish,french,italian,japanese,koreana,polish,brazilian,russian,turkish,simplified chinese,traditional chinese. You will respond with only the correctly translated and formatted output.  Here is an example: INPUT=Glass Bullet Tip  OUTPUT=Glas Kugelspitze, Punta de bala de vidrio, Pointe de balle en verre, Punta di proiettile in vetro, グラス弾丸先端, 유리 총알 팁, Szkło szpicakowe kulka, Ponta de bala de vidro, Стеклянный остроконечный снаряд, Cam takoz ucu, 玻璃子弹尖, 玻璃子彈尖";
 
         // Holds the OpenAIAPI instance for making API calls.
         private OpenAIAPI api;
@@ -57,18 +57,25 @@ namespace LocalizerGUI
             NewFile = new List<Entry>();
 
             // Create a conversation for translation.
-            var chat = api.Chat.CreateConversation();
-            chat.Model = model;
-            chat.AppendSystemMessage(TranslationInstructions);
+            //var chat = api.Chat.CreateConversation();
+            //chat.Model = model;
+            //chat.AppendSystemMessage(TranslationInstructions);
 
-            // Set the first line; the key.
-            NewFile.Add(new Entry() { EnglishText = "Key,File,Type,UsedInMainMenu,NoTranslate,english,Context / Alternate Text,german,spanish,french,italian,japanese,koreana,polish,brazilian,russian,turkish,schinese,tchinese" });
-
+            //// Set the first line; the key.
+            //NewFile.Add(new Entry() { EnglishText = "Key,File,Type,UsedInMainMenu,NoTranslate,english,Context / Alternate Text,german,spanish,french,italian,japanese,koreana,polish,brazilian,russian,turkish,schinese,tchinese" });
+            ;
             // Iterate over each entry and perform translation, skipping the first line.
             for (int i = 1; i < entries.Count; i++)
             {
                 LogManager.Instance().LogMessage($"Translating entry {i} out of {originalFile.Count - 1}");
+                var chat = api.Chat.CreateConversation();
+                chat = api.Chat.CreateConversation();
 
+                chat.Model = model;
+                chat.AppendSystemMessage(TranslationInstructions);
+
+                // Set the first line; the key.
+               // NewFile.Add(new Entry() { EnglishText = "Key,File,Type,UsedInMainMenu,NoTranslate,english,Context / Alternate Text,german,spanish,french,italian,japanese,koreana,polish,brazilian,russian,turkish,schinese,tchinese" });
                 // Append the English text to the chat and get the translation.
                 chat.AppendUserInput(entries[i].EnglishText);
                 var result = await chat.GetResponseFromChatbotAsync();
@@ -76,7 +83,7 @@ namespace LocalizerGUI
                 // Create a new entry using the results and add it to the NewFile list.
                 NewFile.Add(new Entry() { EnglishText = String.Concat(originalFile[i].EnglishText, ",,", result) });
             }
-
+            NewFile.Insert(0, new Entry() { EnglishText = "Key,File,Type,UsedInMainMenu,NoTranslate,english,Context / Alternate Text,german,spanish,french,italian,japanese,koreana,polish,brazilian,russian,turkish,schinese,tchinese" });
             LogManager.Instance().LogMessage("File Translated.");
             FileServices.SaveTranslationToFile(NewFile);
         }
